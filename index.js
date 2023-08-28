19aecb75c63a4b23338eb49e391ccca4a4e57258
SirTokenSol <johnbesco.dev@gmail.com>
2023-08-28 18:03:11 +0100

    Fix nav UI Bugs and mobile responsive queries

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const navToggle = document.querySelector('.menu-button')
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-is-open')
 })
+
+nav.addEventListener('click', () => {
+    document.body.classList.remove('nav-is-open')
+})
